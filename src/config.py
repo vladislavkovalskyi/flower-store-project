542449c1f083ada3910d7011f583e8faf5b00277
vladislavkovalskyi <v.darknesssb@gmail.com>
2024-10-03 22:09:40 +0300

    some changes

--- src/config.py
+++ src/config.py
@@ -7,11 +7,6 @@ env.read_envfile(".env")
 BOT_TOKEN = env.str("BOT_TOKEN")
 OWNER_ID = env.int("OWNER_ID")
 
-DB_USERNAME = env.str("DB_USERNAME")
-DB_PASSWORD = env.str("DB_PASSWORD")
-DB_ADDRESS = env.str("DB_ADDRESS")
-DB_PORT = env.int("DB_PORT")
-
 OWM_TOKEN = env.str("OWM_TOKEN")
 OPENAI_TOKEN = env.str("OPENAI_TOKEN")
 OPENAI_ORGANIZATION = env.str("OPENAI_ORGANIZATION")
